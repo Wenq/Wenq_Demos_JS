6cfda854afd3e1a4ee4d9695d31962fbded79b88
Wenq <wen49880132@163.com>
2018-05-28 23:31:23 +0800

    编译css

--- React_Demos/React全家桶学习/demo1/react-family/src/pages/Page1/Page1.js
+++ React_Demos/React全家桶学习/demo1/react-family/src/pages/Page1/Page1.js
@@ -1,10 +1,11 @@
 /*页面page1*/
 import React,{Component} from 'react';
+import './Page1.css';
 
 export default class Page1 extends Component{
 
 	render(){
-		return <div>
+		return <div className='page-box'>
 		this is Page1!
 		</div>
 	}
--- React_Demos/React全家桶学习/demo1/react-family/webpack.dev.config.js
+++ React_Demos/React全家桶学习/demo1/react-family/webpack.dev.config.js
@@ -18,11 +18,17 @@ module.exports = {
      /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
 	 /*cacheDirectory是用来缓存编译结果，下次编译加速*/
 	 module: {
-	     rules: [{
+	     rules: [
+	     {
 	         test: /\.js$/,
 	         use: ['babel-loader?cacheDirectory=true'],
 	         include: path.join(__dirname, 'src')
-	     }]
+	     },
+	     {
+	     	test: /\.css$/,
+	     	use: ['style-loader','css-loader']
+	     }
+	    ]
 	 },
 
  	// plugins: [
