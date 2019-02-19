8bd844da4c587bf278accf5b4cfc0461353e89fe
Wenq <wen49880132@163.com>
2019-02-19 23:17:44 +0800

    add calss AppWithHooks

---
+++ React_Demos/react_hooks_demo/src/AppWithHooks.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class AppWithHooks extends Component{
+    // constructor(props){
+    //     super(props);
+    // }
+
+    render(){
+        return <div>AppWithHooks</div>
+    }
+}
+
+export default AppWithHooks;
