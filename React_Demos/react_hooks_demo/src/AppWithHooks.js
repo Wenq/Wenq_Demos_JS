cb9d45296b62ac5331482aa5a1252d517e4c4e7b
Wenq <wen49880132@163.com>
2019-02-20 23:04:56 +0800

    refactor class AppWithHooks.js by hooks

--- React_Demos/react_hooks_demo/src/AppWithHooks.js
+++ React_Demos/react_hooks_demo/src/AppWithHooks.js
@@ -1,29 +1,43 @@
 import React, {Component} from 'react';
-import './App.css';
+// import './App.css';
+// class AppWithHooks extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             count: 0
+//         }
+//     }
+
+//     setCount(){
+//         this.setState({
+//             count: ++this.state.count
+//         },()=>{
+//             console.log(`count: +1`);
+//         });
+//     }
+
+//     render(){
+//         return <div>
+//             <span>u click {this.state.count} times</span>
+//             <button onClick={this.setCount.bind(this)}>Click me</button>
+//         </div>
+//     }
+// }
+
+// export default AppWithHooks;
+
+//使用hooks重构
 import {useState} from 'react';
-
-class AppWithHooks extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            count: 0
-        }
-    }
-
-    setCount(){
-        this.setState({
-            count: ++this.state.count
-        },()=>{
-            console.log(`count: +1`);
-        });
-    }
-
-    render(){
-        return <div>
-            <button onClick={this.setCount.bind(this)}>{'点击+1'}</button>
-            <span>{this.state.count}</span>
-        </div>
-    }
+function AppWithHooks() {
+    const [count, setState] = useState(0);
+    return (
+        <div>
+            <p>u click {count} times</p>
+            <button onClick={()=>setState(++count)}>
+                Click me
+            </button>
+            </div>
+    );
 }
 
 export default AppWithHooks;
