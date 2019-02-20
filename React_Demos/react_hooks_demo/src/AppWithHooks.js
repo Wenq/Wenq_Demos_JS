c7656c740f2fe2f7f6f80c71a0c33219b952d5b9
Wenq <wen49880132@163.com>
2019-02-20 22:12:21 +0800

    demo by class

--- React_Demos/react_hooks_demo/src/AppWithHooks.js
+++ React_Demos/react_hooks_demo/src/AppWithHooks.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 import './App.css';
+import {useState} from 'react';
 
 class AppWithHooks extends Component{
-    // constructor(props){
-    //     super(props);
-    // }
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    setCount(){
+        this.setState({
+            count: ++this.state.count
+        },()=>{
+            console.log(`count: +1`);
+        });
+    }
 
     render(){
-        return <div>AppWithHooks</div>
+        return <div>
+            <button onClick={this.setCount.bind(this)}>{'点击+1'}</button>
+            <span>{this.state.count}</span>
+        </div>
     }
 }
 
