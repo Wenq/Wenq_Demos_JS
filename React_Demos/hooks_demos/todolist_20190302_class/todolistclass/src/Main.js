69cb5c2364b4eb59ab5ed32c98e82a4d5e609c71
Wenq <wen49880132@163.com>
2019-03-10 23:47:10 +0800

    update

---
+++ React_Demos/hooks_demos/todolist_20190302_class/todolistclass/src/Main.js
@@ -0,0 +1,18 @@
+/**
+ * 主页组件
+ * 2019-03-10
+ */
+
+ import React,{Component} from 'react';
+//  import * as styles from './Main.css';
+import './Main.css';
+
+ class Main extends Component{
+     render(){
+         return <div className='main'>
+             <span>todolist class 版本实现</span>
+         </div>
+     }
+ }
+
+ export default Main;
