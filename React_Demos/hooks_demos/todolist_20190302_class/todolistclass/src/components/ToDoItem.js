21de4b678404f0b53e9992c05b62735a5bc026ea
Wenq <wen49880132@163.com>
2019-03-17 12:13:20 +0800

    待办项增加删除按钮

--- React_Demos/hooks_demos/todolist_20190302_class/todolistclass/src/components/ToDoItem.js
+++ React_Demos/hooks_demos/todolist_20190302_class/todolistclass/src/components/ToDoItem.js
@@ -1,28 +1,37 @@
 //代办项组件
 import React, { Component } from 'react';
 import './ToDoItem.css';
+import del from './img/del.jpg';
 
 class ToDoItem extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.onCheckChange = this.onCheckChange.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onDelClick = this.onDelClick.bind(this);
     }
 
-    onCheckChange(e){
+    onCheckChange(e) {
+        let { item } = this.props;
+        this.props.onCheckChange && this.props.onCheckChange(item.id);
+    }
+
+    onChange(e) { }
+
+    onDelClick(e) {
         let {item} = this.props;
-        this.props.onCheckChange&&this.props.onCheckChange(item.id);
+        this.props.onChange && this.props.onChange(item.id);
     }
-
-    onChange(e){}
 
     render() {
-        let { style, className, item={}, onCheckChange, ...others } = this.props;
+        let { style, className, item = {}, onCheckChange, ...others } = this.props;
         return <div className={`todoitem ${className}`} style={style} {...others}>
-            <input type='checkbox' className={'todoitem_check'} onClick={this.onCheckChange} 
-            checked={item.do} onChange={this.onChange}/>
+            <input type='checkbox' className={'todoitem_check'} onClick={this.onCheckChange}
+                checked={item.do} onChange={this.onChange} />
             <span className='todoitem_title'>{item.title || '无'}</span>
             <span className='todoitem_date'>{item.date || '3/14'}</span>
+            <img alt='delete' className={'todoitem_del'} src={del} onClick={this.onDelClick} />
         </div>
     }
 }
