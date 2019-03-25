0ba436b3b4c3d9395c774fbb435323372eb45d2f
Wenq <wen49880132@163.com>
2019-03-25 23:21:53 +0800

    用Provider组件包裹根组件Main，从而通过context传递store

--- React_Demos/hooks_demos/todolist_20190302_class/todolistclass/src/index.js
+++ React_Demos/hooks_demos/todolist_20190302_class/todolistclass/src/index.js
@@ -6,12 +6,22 @@ import * as serviceWorker from './serviceWorker';
 
 import Main from './Main';
 import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 import { reducer_actions } from './reducer/reducer';
+import { List } from 'immutable';
 
-const store = createStore(reducer_actions);
+const store = createStore(reducer_actions, new List());
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Main style={{ 'padding': '10px' }} />, document.getElementById('root'));
+
+//根组件Main
+// ReactDOM.render(<Main style={{ 'padding': '10px' }} />, document.getElementById('root'));
+
+//使用react-redux包裹
+ReactDOM.render(<Provider store={store}>
+    <Main style={{ 'padding': '10px' }} />>
+</Provider>,document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
