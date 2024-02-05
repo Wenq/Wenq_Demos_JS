d62517a418473dd1ace0f55d466e6cca9a2f6cca
wenq <wen49880132@163.com>
2024-02-05 17:57:42 +0800

    新建chrome插件，支持复制当前页签url并在新建浏览器页签中直接打开
    Feat: xx

---
+++ ChromePlugins_Dev/CopyUrlAndCreateNewTab/popup/index.js
@@ -0,0 +1,15 @@
+const plugin_search_but = document.getElementById('plugin_search_but')
+plugin_search_but.onclick = async function () {
+    // 这是一个基本的 Chrome 扩展程序示例
+    // 它会在工具栏中添加一个按钮，点击该按钮会打开一个新的标签页，并导航到当前活动标签页的 URL
+
+    // 获取当前窗口的tab
+    const tabs = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+    if (tabs && tabs.length > 0) {
+        // 创建一个新的标签页，并导航到当前活动标签页的 URL
+        chrome.tabs.create({ url: tabs[0].url });
+    }
+}
